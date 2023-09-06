ec67010ba52163a90eee64aa106aa7d514cf2bf6
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-06 18:38:25 +0200

    semi-standard style fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
-const util = require('util')
-const request = util.promisify(require('request'))
-const movieId = process.argv[2]
+const util = require('util');
+const request = util.promisify(require('request'));
+const movieId = process.argv[2];
 
 async function StarWarsCharacters(movieId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + movieId
-  let response = await (await request(endpoint)).body
-  response = JSON.parse(response)
-  const characters = response.characters
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
   for (let i = 0; i < characters.length; i++) {
-    const characterUrl = characters[i]
-    let character = await(await request(characterUrl)).body
-    character = JSON.parse(character)
-    console.log(character.name)
+    const characterUrl = characters[i];
+    let character = await(await request(characterUrl)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
   }
 }
 
-StarWarsCharacters(movieId)
+StarWarsCharacters(movieId);
