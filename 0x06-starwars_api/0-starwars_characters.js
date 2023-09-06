18a8801770f6557aad45fdb1643e6f17e63869a2
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-06 12:16:55 +0200

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const util = require('util')
+const request = util.promisify(require('request'))
+const movieId = process.argv[2]
+
+if (!movieId) {
+  process.exit(1)
+}
+
+async function StarWarsCharacters(movieId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId
+  let response = await (await request(endpoint)).body
+  response = JSON.parse(response)
+  const characters = response.characters
+
+  for (let i = 0; i < characters.length; i++) {
+    const characterUrl = characters[i]
+    let character = await(await request(characterUrl)).body
+    character = JSON.parse(character)
+    console.log(character.name)
+  }
+}
+
+StarWarsCharacters(movieId)
