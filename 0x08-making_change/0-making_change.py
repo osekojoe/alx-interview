0513b75ce8eedb221142b47fade39c7b804f8f58
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-19 17:09:08 +0200

    efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,22 +9,17 @@ Given a pile of coins of different values, determine
 def makeChange(coins, total):
     if total <= 0:
         return 0
-
-    # Initialize a list to store the fewest number of coins for each amount from 0 to total.
-    # Initialize all values to a large number (infinity) initially.
-    dp = [float('inf')] * (total + 1)
-
-    # For 0 total, the fewest number of coins needed is 0.
-    dp[0] = 0
-
-    # Iterate through each coin denomination.
-    for coin in coins:
-        # Update the dp array for each possible amount from the coin value up to total.
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    # If dp[total] is still infinity, it means the total cannot be met by any combination of coins.
-    if dp[total] == float('inf'):
-        return -1
-
-    return dp[total]
+    remainder = total
+    count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while remainder > 0:
+        if coin_idx >= n:
+            return -1
+        if remainder - sorted_coins[coin_idx] >= 0:
+            remainder -= sorted_coins[coin_idx]
+            count += 1
+        else:
+            coin_idx += 1
+    return count
